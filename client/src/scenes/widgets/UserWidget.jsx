fa22cb2104c062cd627645d9128fc7ce743c7f36
Hanrocky <hanrocky.halim@gmail.com>
2023-01-07 00:32:41 +0700

    Add UserWidget

---
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -0,0 +1,32 @@
+import {
+    ManageAccountsOutlined,
+    EditOutlined,
+    LocationOnOutlined,
+    WorkOutlineOutlined,
+} from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import UserImage from "components/UserImage";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const UserWidget = ({ userId, picturePath }) => {
+    const [ user, setUser ] = useState(null);
+    const { palette } = useTheme();
+    const navigate = useNavigate();
+    const token = useSelector((state) => state.token);
+    const dark = palette.neutral.dark;
+    const medium = palette.neutral.medium;
+    const main = palette.neutral.main;
+
+    const getUser = async () =>{
+        const response = await fetch(`http://localhost:3001/users/${userId}`,
+            {
+                method: "GET",
+                headers: { Authorization: `Bearer ${token}`},
+            }
+        )
+    }
+}
